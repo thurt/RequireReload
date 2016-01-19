ca3b3994f96b6d953b14151736c66f4cadba9ab8
Taylor Hurt <taylor.a.hurt@gmail.com>
2016-01-19 17:03:22 +0000

    patch: require module memory leak

--- bin/rerequire.js
+++ bin/rerequire.js
@@ -1,8 +1,7 @@
 #!/usr/bin/env node
 'use strict'
 
-var repl = require('repl').start({ useGlobal: true })
+require('repl').start({ useGlobal: true })
 
 require('../lib/index.js')
 // you could also require lib/index.js in your own repl
-
