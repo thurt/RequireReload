ef38476b9a5d79660f93bb79f42e7d5aac94218d
Taylor Hurt <taylor.a.hurt@gmail.com>
2016-01-19 06:15:40 +0000

    patch: adds cwd paths to rerequire module on load

--- bin/rerequire.js
+++ bin/rerequire.js
@@ -1,52 +1,8 @@
 #!/usr/bin/env node
 'use strict'
-var fs = require('fs')
-var repl = require('repl').start({})
 
-var _modules = {} // { module: _var, ... }
-// I store all the rerequired modules here.
-// the key's value is the global variable associated with the module
+var repl = require('repl').start({ useGlobal: true })
 
-repl.context.rerequire = (module_name, var_name) => {
-  let module = require.resolve(module_name)
-  let _var = null
+require('../lib/index.js')
+// you could also require lib/index.js in your own repl
 
-  for (let _module in _modules) {
-    if (_module === module) {
-      _var = _modules[_module]
-      break
-    }
-  }
-
-  if (_var) {
-    console.log(`[rerequire]: ${module_name} is already rerequired at variable '${_var}'`)
-  }
-
-  if (!_var) {
-    let fsTimeout
-
-    _modules[module] = var_name
-
-    fs.watch(module, (event, filename) => {
-    // << This could be a source of bugs >>
-    // fs.watch on linux uses inotify -- which will send two change events on each file save
-    // I believe one of these is MODIFY and one is CLOSE_WRITE
-    // I can't tell the difference from within Node, so I'm putting a timeout 1 sec
-    // so that the second change call will not do anything -- "if (!fsTimeout)"
-      if (!fsTimeout && event === 'change') {
-        delete repl.context.require.cache[module]
-        try {
-          repl.context.global[var_name] = require(module)
-        } finally {
-          // if there is a syntax error from require(module)
-          // I still want to make sure that the fsTimeout gets set.
-          // this finally block ensures the second fs.watch call always gets blocked
-          // regardless of a syntax error or not.
-          fsTimeout = setTimeout(() => fsTimeout = null, 1000)
-        }
-      }
-    })
-
-    repl.context.global[var_name] = require(module)
-  }
-}
